bb3a99e7a680fb8b61ad84adda4d9fa45d8faa09
Charles Miller <chmomiller@gmail.com>
2020-06-03 16:39:39 -0700

    CSS changes and scroll reveal

--- js/index.js
+++ js/index.js
@@ -1,14 +1,3 @@
-ScrollReveal().reveal('.headline');
-ScrollReveal().reveal('.dont', { delay: 100 });
-ScrollReveal().reveal('.tagline', { delay: 100 });
-ScrollReveal().reveal('.punchline', { delay: 100 });
-ScrollReveal().reveal('.sew', { delay: 100 });
-ScrollReveal().reveal('.title3', {delay: 100 });
-ScrollReveal().reveal('.downfall', {delay: 200 });
-ScrollReveal().reveal('.h2', {delay: 200 });
-ScrollReveal().reveal('.column', {delay: 200 });
-
-
-function goBack() {
-  window.history.back();
-}
+ScrollReveal().reveal('.header');
+ScrollReveal().reveal('.paragraph', { delay: 100 });
+ScrollReveal().reveal('.list', { delay: 100 });
--- sierra/new.js
+++ sierra/new.js
@@ -1,13 +1,6 @@
-ScrollReveal().reveal('.headline');
-ScrollReveal().reveal('.dont', { delay: 100 });
-ScrollReveal().reveal('.tagline', { delay: 100 });
-ScrollReveal().reveal('.punchline', { delay: 100 });
-ScrollReveal().reveal('.sew', { delay: 100 });
-ScrollReveal().reveal('.title3', {delay: 100 });
-ScrollReveal().reveal('.downfall', {delay: 100 });
-ScrollReveal().reveal('.h2', {delay: 100 });
-ScrollReveal().reveal('.column', {delay: 100 });
-
+ScrollReveal().reveal('.h1');
+ScrollReveal().reveal('.p', { delay: 100 });
+ScrollReveal().reveal('.ul', { delay: 100 });
 
 function goBack() {
   window.history.back();
