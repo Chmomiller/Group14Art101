375d79b542bd612b0c2049022287421410ef1bc9
Charles Miller <chmomiller@gmail.com>
2020-06-03 16:21:52 -0700

    Js changes

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@ ScrollReveal().reveal('.tagline', { delay: 100 });
 ScrollReveal().reveal('.punchline', { delay: 100 });
 ScrollReveal().reveal('.sew', { delay: 100 });
 ScrollReveal().reveal('.title3', {delay: 100 });
-ScrollReveal().reveal('.downfall', {delay: 100 });
-ScrollReveal().reveal('.h2', {delay: 100 });
-ScrollReveal().reveal('.column', {delay: 100 });
+ScrollReveal().reveal('.downfall', {delay: 200 });
+ScrollReveal().reveal('.h2', {delay: 200 });
+ScrollReveal().reveal('.column', {delay: 200 });
 
 
 function goBack() {
