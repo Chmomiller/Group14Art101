8e6ad861dba2f213a09188a2b858493e31ee81b1
Katheriya Prowsri <50426080+kprowsri@users.noreply.github.com>
2020-06-06 14:45:13 -0700

    changed bg color and added scroll reveal

---
+++ katTShirtBandanaMask/js/tshirtbandana.js
@@ -0,0 +1,12 @@
+/*
+	*Author: Katheriya Prowsri <kprowsri@ucsc.edu>
+	*Created: 6 June 2020
+	*License: Public Domain
+*/
+
+//scroll through website to reveal parts of page
+ScrollReveal().reveal('#content');
+
+function goBack() {
+  window.history.back();
+}
