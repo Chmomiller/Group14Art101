89b74b030357b980ccb2bda8a375a5a183ad1234
sierracinnamon <65202668+sierracinnamon@users.noreply.github.com>
2020-06-06 14:06:39 -0700

    new project folder

---
+++ sierra/project/new.js
@@ -0,0 +1,20 @@
+
+//this is the JS used from the Scroll to Reveal Library
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.dont', {delay: 100 });
+ScrollReveal().reveal('.tagline', { delay: 200 });
+ScrollReveal().reveal('.punchline', { delay: 200});
+ScrollReveal().reveal('.sew', {delay:300});
+ScrollReveal().reveal('.title3', {delay: 350});
+ScrollReveal().reveal('.downfall', {delay: 400});
+ScrollReveal().reveal('.h2', {delay: 400});
+ScrollReveal().reveal('.column', {delay:450});
+ScrollReveal().reveal('.day', {delay:400});
+ScrollReveal().reveal('.day2', {delay:450});
+ScrollReveal().reveal('.age', {delay:400});
+ScrollReveal().reveal('.age2', {delay:450});
+
+//this is for the button to go back to the previous page
+function goBack() {
+  window.history.back();
+}
