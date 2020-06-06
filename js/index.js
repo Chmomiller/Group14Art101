f301ca19783d8ccd3d4cd569c19d8fca9fe332e6
Katheriya Prowsri <50426080+kprowsri@users.noreply.github.com>
2020-06-06 16:19:07 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -27,8 +27,9 @@ ScrollReveal().reveal('.list', { delay: 100 });
 
 			        //setting the data to variables
 			        var caseNumEl = $('<p>').html(data.reports[0].cases);
+			        var caseNumTitle = $('<h2>').html(COVID-19 Global Cases RealTime)
 			        //appending to output
-			        $("#covid-api").append($('<h2>').html(COVID-19 Global Cases RealTime));
+			        $("#covid-api").append(caseNumTitle);
 			        $("covid-api").append(caseNumEl);
 			        
 			    },
