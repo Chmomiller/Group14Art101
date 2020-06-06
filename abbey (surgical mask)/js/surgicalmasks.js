46c4df5be7beefdd9f372db1ba7eed74f1480a18
abbeycarmel <adcarmel@ucsc.edu>
2020-06-06 14:31:28 -0700

    update js

--- abbey (surgical mask)/js/surgicalmasks.js
+++ abbey (surgical mask)/js/surgicalmasks.js
@@ -1,6 +1,4 @@
-ScrollReveal().reveal('.h1');
-ScrollReveal().reveal('.p', { delay: 100 });
-ScrollReveal().reveal('.ul', { delay: 100 });
+ScrollReveal().reveal('#content1');
 
 function goBack() {
   window.history.back();
