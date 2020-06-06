b9300c3352e3deeb1d1cb93ce6197199d00c9d6b
rachaelsbach <relsbach@ucsc.edu>
2020-06-06 14:34:18 -0700

    commit

--- n95/js/new.js
+++ n95/js/new.js
@@ -1,6 +1,13 @@
+<<<<<<< Updated upstream
 ScrollReveal().reveal('.h1');
 ScrollReveal().reveal('.p', { delay: 100 });
 ScrollReveal().reveal('.ul', { delay: 100 });
+=======
+ScrollReveal().reveal('#content1', { delay: 100});
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.sew', { delay: 200 });
+ScrollReveal().reveal('.dont', { delay: 200 });
+>>>>>>> Stashed changes
 
 function goBack() {
   window.history.back();
