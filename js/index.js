eb3e647edfdbf6e6473cd02a8421077d120f9cc1
Katheriya Prowsri <50426080+kprowsri@users.noreply.github.com>
2020-06-06 20:21:33 -0700

    Update index.js
    api hopefully all done!!

--- js/index.js
+++ js/index.js
@@ -1,51 +1,56 @@
+//Template Code before from Wes Modes - Lab 16 Assignment page*/
+//Source of API: https://github.com/ChrisMichaelPerezSantiago/covid19
+//COVID-19 RealTime Cases Tracker API credited to Chris Michael
 
-//adding the scroll reveal so that the ids slowly show up.
-//source: https://scrollrevealjs.org/guide/installation.html
-/*ScrollReveal().reveal('.header');
-ScrollReveal().reveal('.paragraph', { delay: 100 });
-ScrollReveal().reveal('.list', { delay: 100 });
+// Using the core $.ajax() method
+	$.ajax({
+	    // The URL for the request (from the api docs)
+	    url: "https://covid19-server.chrismichael.now.sh/api/v1/AllReports",
+	    // The data to send (will be converted to a query string)
+	    data: { },
+	    // Whether this is a POST or GET request
+	    type: "GET",
+	    // The type of data we expect back
+	    dataType : "json",
+	    // What do we do when the api call is successful
+	    //   all the action goes in here
+	    success: function(data) {
+	        console.log(data);
 
-//API that is a COVID-19 case tracker.
-	/*function getImage(url) {
-		$('#output').empty(); //clears the page every time
+	        //setting the data to variables
+	        var caseNumEl = data.reports[0].cases;
+	        var deathNumEl = data.reports[0].deaths;
+	        var recoveredNumEl = data.reports[0].recovered;
+	        var activeNumEl = data.reports[0].active_cases[0].currently_infected_patients;
+	        //testing to see if correct number is showing
+	        console.log(caseNumEl);
+	        console.log(deathNumEl);
+	        console.log(recoveredNumEl);
+	        console.log(activeNumEl);
 
-			//Template Code before from Wes Modes - Lab 16 Assignment page*/
-			// Using the core $.ajax() method
-			$.ajax({
-			    // The URL for the request (from the api docs)
-			    url: "https://covid19-server.chrismichael.now.sh/api/v1/AllReports",
-			    // The data to send (will be converted to a query string)
-			    data: { },
-			    // Whether this is a POST or GET request
-			    type: "GET",
-			    // The type of data we expect back
-			    dataType : "json",
-			    // What do we do when the api call is successful
-			    //   all the action goes in here
-			    success: function(data) {
-			        console.log(data);
+	        //nice labelling text
+	        var caseNumTitle = $('<h1>').text("COVID-19 Global Cases RealTime");
+	        var caseDescript = $('<h2>').text("# of Cases");
+	        var deathDescript = $('<h2>').text("# of Deaths");
+			var recoveredDescript = $('<h2>').text("# of Recovered");
+			var activeDescript = $('<h2>').text("# of Active Cases");
 
-			        //setting the data to variables
-			        var caseNumEl = data.reports[0].cases;
-			        var deathNumEl = data.reports[0].deaths;
-			        var recoveredNumEl = data.reports[0].recovered;
-			        var activeNumEl = data.reports[0].active_cases[0].currently_infected_patients;
-			        //testing to see if correct number is showing
-			        console.log(caseNumEl);
-			        console.log(deathNumEl);
-			        console.log(recoveredNumEl);
-			        console.log(activeNumEl);
 
-			        var caseNumTitle = $('<h1>').text("COVID-19 Global Cases RealTime");
-
-			        //appending to output
-			        $("#covid-api").append(caseNumTitle);
-			        $("#covid-api").append($('<p>').html(caseNumEl));
-			        
-			    },
-			    // What we do if the api call fails
-			    error: function (jqXHR, textStatus, errorThrown) { 
-			        // do stuff
-			        console.log("Error:", textStatus, errorThrown);
-			    }
-			});
+	        //appending to output
+	        $("#covid-api").append(caseNumTitle);
+	        $("#covid-api").append(caseDescript);
+	        $("#covid-api").append($('<p>').html(caseNumEl));
+	        $("#covid-api").append(deathDescript);
+	        $("#covid-api").append($('<p>').html(deathNumEl));
+	        $("#covid-api").append(recoveredDescript);
+	        $("#covid-api").append($('<p>').html(recoveredNumEl));
+	        $("#covid-api").append(activeDescript);
+	        $("#covid-api").append($('<p>').html(activeNumEl));
+	        
+	    },
+	    // What we do if the api call fails
+	    error: function (jqXHR, textStatus, errorThrown) { 
+	        // do stuff
+	        console.log("Error:", textStatus, errorThrown);
+	    }
+	});
